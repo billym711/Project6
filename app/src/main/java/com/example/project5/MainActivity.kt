510abbd8f5236229ffae4b485cb8718ac00b73c2
billym711 <bmoorekid@gmail.com>
2023-10-03 17:55:03 -0400

    Translation now happens on any text update

--- app/src/main/java/com/example/project5/MainActivity.kt
+++ app/src/main/java/com/example/project5/MainActivity.kt
@@ -2,7 +2,9 @@ package com.example.project5
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-
+/**
+ * Instantiates the main layout
+ */
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
